813d455536197f48e49535b446e9863dbf5aeb59
mukesh <mukesh@MUKESH-PC>
2016-12-26 04:54:50 +0000

    Updated message

--- helloworld/Controllers/HomeController.cs
+++ helloworld/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace helloworld.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Hello World.";
 
             return View();
         }
